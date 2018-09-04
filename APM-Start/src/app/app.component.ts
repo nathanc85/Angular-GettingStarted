ea938d6ef482a8d4d339a7e322630f4c98b2d126
Nathan Costea <cnlnat@yahoo.com>
2018-09-04 16:05:01 -0400

    changed the default of a property

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular: Getting Started Now';
-  weekDay: string = 'Monday';
+  weekDay: string = 'Friday';
 }
